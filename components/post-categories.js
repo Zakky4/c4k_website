f835798e3c32937e3c7eca56cd4c211fdad6c820
zakky <sadaoza@gmail.com>
2024-06-28 20:28:15 +0900

    get categories error

--- components/post-categories.js
+++ components/post-categories.js
@@ -1,7 +1,7 @@
-import styles from 'styles/post-categories.module.css'
-import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFolderOpen } from '@fortawesome/free-regular-svg-icons'
+import styles from "styles/post-categories.module.css";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderOpen } from "@fortawesome/free-regular-svg-icons";
 
 export default function PostCategories({ categories }) {
   return (
@@ -11,13 +11,15 @@ export default function PostCategories({ categories }) {
         <span className="sr-only">Categories</span>
       </h3>
       <ul className={styles.list}>
-        {categories.map(({ name, slug }) => (
-          <li key={slug}>
-            <Link href={`/blog/category/${slug}`}>
-              {name}
-            </Link>
-          </li>
-        ))}
+        {categories && categories.length > 0 ? (
+          categories.map(({ name, slug }) => (
+            <li key={slug}>
+              <Link href={`/blog/category/${slug}`}>{name}</Link>
+            </li>
+          ))
+        ) : (
+          <li>カテゴリがありません</li>
+        )}
       </ul>
     </div>
   );
