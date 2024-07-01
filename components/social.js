1c8aa9729ad2598edd8720b306c6e3f8b58ffa54
zakky <sadaoza@gmail.com>
2024-07-02 06:59:13 +0900

    update social icons

--- components/social.js
+++ components/social.js
@@ -2,7 +2,7 @@ import styles from 'styles/social.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faTwitter,
+  faXTwitter,
   faFacebookF,
   faGithub,
 } from '@fortawesome/free-brands-svg-icons'
@@ -11,19 +11,19 @@ export default function Social({ iconSize = 'initial' }) {
   return (
     <ul className={styles.list} style={{ '--icon-size': iconSize }}>
       <li>
-        <a href="https://twitter.com/">
-          <FontAwesomeIcon icon={faTwitter} />
+        <a href="https://x.com/zakkyweb1" target='_blank'>
+          <FontAwesomeIcon icon={faXTwitter} />
           <span className="sr-only">Twitter</span>
         </a>
       </li>
-      <li>
+      {/* <li>
         <a href="https://www.facebook.com/">
           <FontAwesomeIcon icon={faFacebookF} />
           <span className="sr-only">Facebook</span>
         </a>
-      </li>
+      </li> */}
       <li>
-        <a href="https://github.com/">
+        <a href="https://github.com/Zakky4" target='_blank'>
           <FontAwesomeIcon icon={faGithub} />
           <span className="sr-only">GitHub</span>
         </a>
