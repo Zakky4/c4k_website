e5fee243dab12101cc607f7445a42ce81d9947ab
zakky <sadaoza@gmail.com>
2024-07-02 13:30:47 +0900

    edit nexconfig

--- next.config.js
+++ next.config.js
@@ -1,34 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     // domains: ['images.microcms-assets.io'],
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.microcms-assets.io',
-//         port: '',
-//         pathname: '/assets/xxxxxxxxxx/**',
-//       },
-//     ],
-//   },
-//   eslint: { // eslintのlint checkをbuild時にoff
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: { // type checkをbuild時にoff
-//     ignoreBuildErrors: true,
-//   }
-// }
-
-// module.exports = {
-//   images: {
-//     domains: ['images.microcms-assets.io'],
-//   },
-//   env: {
-//     MY_VARIABLE: process.env.MY_VARIABLE,
-//   },
-//   output: 'export',
-// };
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -42,7 +12,19 @@ const nextConfig = {
       },
     ],
   },
-  output: 'export',
+  eslint: { // eslintのlint checkをbuild時にoff
+    ignoreDuringBuilds: true,
+  },
+  typescript: { // type checkをbuild時にoff
+    ignoreBuildErrors: true,
+  }
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['images.microcms-assets.io'],
+  },
+  env: {
+    MY_VARIABLE: process.env.MY_VARIABLE,
+  },
+};
