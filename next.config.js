f8cbc9fac2a17cee68d87489e8654b7f29715fc9
zakky <sadaoza@gmail.com>
2024-07-02 20:12:39 +0900

    update nextconfig

--- next.config.js
+++ next.config.js
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     // domains: ['images.microcms-assets.io'],
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'images.microcms-assets.io',
-//         port: '',
-//         pathname: '/assets/xxxxxxxxxx/**',
-//       },
-//     ],
-//   },
-//   eslint: { // eslintのlint checkをbuild時にoff
-//     ignoreDuringBuilds: true,
-//   },
-//   typescript: { // type checkをbuild時にoff
-//     ignoreBuildErrors: true,
-//   }
-// }
 
-module.exports = {
-  // output: 'export',
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/assets/xxxxxxxxxx/**',
+      },
+    ],
+  },
+  eslint: {
+    // eslintのlint checkをbuild時にoff
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // type checkをbuild時にoff
+    ignoreBuildErrors: true,
   },
   env: {
-    MY_VARIABLE: process.env.MY_VARIABLE,
+    API_KEY: process.env.API_KEY,
+    SERVICE_DOMAIN: process.env.SERVICE_DOMAIN,
   },
 };
+
+module.exports = nextConfig;
