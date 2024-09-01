d36f28723a5cd7689bdf1104a5e247e6ca7a8bbe
zakky <sadaoza@gmail.com>
2024-09-01 13:40:59 +0900

    Chage Hero Image

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,6 @@
 import styles from 'styles/hero.module.css'
 import Image from "next/legacy/image";
-import cube from 'images/cube.jpg'
-import c4k from 'images/c4k.png'
+import coding from 'images/coding.png'
 
 export default function Hero({ title, subtitle, imageOn = false }) {
   return (
@@ -13,7 +12,7 @@ export default function Hero({ title, subtitle, imageOn = false }) {
       {imageOn && (
         <figure className={styles.image}>
           <Image
-            src={c4k}
+            src={coding}
             alt=""
             layout="responsive"
             sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
